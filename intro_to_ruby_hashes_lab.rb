79180162e9605bc9dbe23c6fb1c6f5399f666078
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2019-12-19 17:31:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,7 +29,12 @@ def monopoly_with_second_tier
   :railroads=>
   {
     :pieces=>4,
-    
+    :rent_in_dollars=>
+    {
+      :one_piece_owned=>25,
+      :two_pieces_owned=>50,
+      :three_pieces_owned=>100,
+      :four_pieces_owned=>200
     
   }
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
@@ -55,5 +60,6 @@ end
 
 def monopoly_with_fourth_tier
 	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
+	
 
 end
